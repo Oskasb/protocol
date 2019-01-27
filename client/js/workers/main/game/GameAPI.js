4045bdc48864c1677c6ec1a574952347c8816963
oskar <oskar.asbrink@gmail.com>
2019-01-27 15:59:46 +0100

    model details

--- client/js/workers/main/game/GameAPI.js
+++ client/js/workers/main/game/GameAPI.js
@@ -29,7 +29,7 @@ define([
         var testWeapon;
 
         var slots = ['GRIP_R', 'PROP_2', 'PROP_3'];
-        var skinItems = ["SHIRT_CHAIN", "SHIRT_SCALE", "LEGS_CHAIN","LEGS_SCALE"];
+        var skinItems = ["SHIRT_CHAIN", "SHIRT_SCALE", "LEGS_CHAIN","LEGS_SCALE", 'BOOTS_SCALE', 'GLOVES_SCALE'];
 
         GameAPI.loadTestPiece = function() {
             testPiece = !testPiece;
